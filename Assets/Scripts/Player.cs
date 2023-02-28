5b8cc4c55a542f7d360983940a05e9537818eb91
TalisTalis <tsarst@gmail.com>
2023-02-28 07:56:13 +0300

    start

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 2.5f;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector2.left);
+            transform.Translate(Vector2.left * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * speed);
         }
     }
 }
